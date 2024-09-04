814986080fc64bd69b8cc9dbb8ce5d58d8af4170
SATHILIN <sathilin@ford.com>
2024-09-04 14:32:37 +0530

    first commit

---
+++ src/main/java/com/jenkins/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.jenkins.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping(value = "hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello");
+    }
+
+}
